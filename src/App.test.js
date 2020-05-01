ad8d62b8a477f0f786e60ae6b3c52f37ff73f45b
Alan Willoughby <satyadvaya@gmail.com>
2020-05-01 06:22:48 -0700

    added Guide.App.js and data.js files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
+test('test dynamic renders', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('div').length).toBe(1);
+});
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
---
+++ src/data.js
@@ -0,0 +1,144 @@
+const bestiary = [
+    {
+        pokemon: "VENUSAUR",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        shape: "Shape: Quadruped",
+        attack: "Attack: 82",
+        defense: "Defense: 83"
+    },
+    {
+        pokemon: "BULBASAUR",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        shape: "Shape: Quadruped",
+        attack: "Attack: 49",
+        defense: "Defense: 49"
+    },
+    {
+        pokemon: "VENUSAUR",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/003_f2.png",
+        shape: "Shape: NA",
+        attack: "Attack: 100",
+        defense: "Defense: 123"
+    },
+    {
+        pokemon: "IVYSAUR",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+        shape: "Shape: Quadruped",
+        attack: "Attack: 62",
+        defense: "Defense: 63"
+    },
+    {
+        pokemon: "CHARMANDER",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 52",
+        defense: "Defense: 43"
+    },
+    {
+        pokemon: "CHARMELEON",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 64",
+        defense: "Defense: 58"
+    },
+    {
+        pokemon: "CHARIZARD",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 84",
+        defense: "Defense: 78"
+    },
+    {
+        pokemon: "CHARIZARD-MEGA-X",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/006_f2.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 84",
+        defense: "Defense: 78"
+    },
+    {
+        pokemon: "CHARIZARD-MEGA-Y",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/006_f3.png",
+        shape: "Shape: NA",
+        attack: "Attack: 104",
+        defense: "Defense: 78"
+    },
+    {
+        pokemon: "SQUIRTLE",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 48",
+        defense: "Defense: 65"
+    },
+    {
+        pokemon: "WARTORTLE",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 63",
+        defense: "Defense: 80"
+    },
+    {
+        pokemon: "BLASTOISE",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+        shape: "Shape: Upright",
+        attack: "Attack: 83",
+        defense: "Defense: 100"
+    },
+    {
+        pokemon: "BLASTOISE-MEGA",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/009_f2.png",
+        shape: "Shape: NA",
+        attack: "Attack: 103",
+        defense: "Defense: 120"
+    },
+    {
+        pokemon: "CATERPIE",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+        shape: "Shape: Squiggle",
+        attack: "Attack: 30",
+        defense: "Defense: 35"
+    },
+    {
+        pokemon: "METAPOD",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png",
+        shape: "Shape: Squiggle",
+        attack: "Attack: 20",
+        defense: "Defense: 55"
+    },
+    {
+        pokemon: "BEEDRILL",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png",
+        shape: "Shape: Bug-Wings",
+        attack: "Attack: 90",
+        defense: "Defense: 40"
+    },
+    {
+        pokemon: "WEEDLE",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png",
+        shape: "Shape: Squiggle",
+        attack: "Attack: 35",
+        defense: "Defense: 30"
+    },
+    {
+        pokemon: "KAKUNA",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png",
+        shape: "Shape: Squiggle",
+        attack: "Attack: 25",
+        defense: "Defense: 50"
+    },
+    {
+        pokemon: "BEEDRILL-MEGA",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/015_f2.png",
+        shape: "Shape: NA",
+        attack: "Attack: 150",
+        defense: "Defense: 40"
+    },
+    {
+        pokemon: "PIDGEY",
+        url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png",
+        shape: "Shape: Wings",
+        attack: "Attack: 45",
+        defense: "Defense: 40"
+    }
+];
+
+export default bestiary;
