f1aad4744bc93edd079bb468a1a25c0c636862be
Alan Willoughby <satyadvaya@gmail.com>
2020-05-04 09:59:34 -0700

    added styling to Detail.css

--- src/Card.js
+++ src/Card.js
@@ -7,11 +7,11 @@ export default class Card extends Component {
             <div className='card'>
                 <Link to={`/pokemon/${this.props.pokemon._id}`}>
                     <h2>Name: {this.props.pokemon.pokemon}</h2>
+                    </Link>
                     <img src={this.props.pokemon.url_image} alt={this.props.pokemon.pokemon} />
                     <h3>Shape: {this.props.pokemon.shape}</h3>
                     <h3>Attack: {this.props.pokemon.attack}</h3>
                     <h3>Defense: {this.props.pokemon.defense}</h3>
-                </Link>
             </div>
         )
     }
