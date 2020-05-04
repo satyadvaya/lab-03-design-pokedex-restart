4a0e4ba6c020cbf7e3f97091e296b3e2b6d7c983
Alan Willoughby <satyadvaya@gmail.com>
2020-05-03 21:55:44 -0700

    added passing tests

---
+++ src/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App.js';
+import Card from './Card.js';
+
+test('test dynamic renders', () => {
+  const wrapper = shallow(<Card pokemon={{}} />);
+  expect(wrapper.find('div').length).toBe(1);
+});
+
+test('renders learn react link', () => {
+  const wrapper = shallow(<Card pokemon={{}} />);
+  expect(wrapper).toMatchSnapshot();
+});
